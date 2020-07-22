2517dd54fa6b3d04b924ae466eeb03255d995236
Guetso <legalloisprovensal@gmail.com>
2020-07-22 22:22:14 +0200

    ok responsive 'canvas'

---
+++ src/components/Nav.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="navBar">
+    <button class="navBar__upBtn"></button>
+    <nav>
+      <ul class="navBar__items">
+        <li class="navBar__item" v-for="item in navItems" :key="item.id">{{item.text}}</li>
+      </ul>
+    </nav>
+    <button class="navBar__downBtn"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data: function () {
+    return {
+      navItems: [
+        { id: 1, text: 'about me', icon: 'message' },
+        { id: 2, text: 'contact me', icon: 'message' },
+        { id: 3, text: 'portfolio', icon: 'message' },
+        { id: 4, text: 'skills', icon: 'message' }
+      ]
+    }
+  }
+}
+</script>
